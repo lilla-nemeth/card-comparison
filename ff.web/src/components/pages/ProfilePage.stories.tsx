f60201cd55dc7f71b33c14206413321018f64b07
Pasi J <pasiaj@vuo.ai>
2024-09-06 17:36:43 +0300

    Add base pages

---
+++ ff.web/src/components/pages/ProfilePage.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ProfilePage from "./ProfilePage";
+
+const meta: Meta<typeof ProfilePage> = {
+  title: "Pages/ProfilePage",
+  component: ProfilePage,
+  tags: ["autodocs"],
+};
+export default meta;
+
+type Story = StoryObj<typeof ProfilePage>;
+
+export const DefaultProfilePage: Story = {
+  args: {},
+};
---
+++ ff.web/src/components/pages/ProfilePage.tsx
@@ -0,0 +1,16 @@
+import Page from "@vuo/templates/Page";
+import Button from "@vuo/atoms/Button";
+import FixFoodLogo from "@vuo/atoms/FixFoodLogo";
+import { LogoVariants } from "@vuo/utils/LogoUtils";
+
+const ProfilePage = function (props: any) {
+  return (
+    <Page containerClass="flex flex-col">
+      <div className="home flex flex-col">
+        <p>This is an empty page.</p>
+      </div>
+    </Page>
+  );
+};
+
+export default ProfilePage;
