31076c32336e49574732044a8f7e391992b7862b
Lilla Németh <naemly@protonmail.com>
2024-10-03 11:30:35 +0300

    updating initialTheme condition with undefined to eliminate error in setTheme

--- ff.web/src/context/ThemeContext.tsx
+++ ff.web/src/context/ThemeContext.tsx
@@ -1,27 +1,34 @@
-import "../scss/app.scss"
+import "../scss/app.scss";
 
 interface ThemeContextProps {
-  theme: 'light-theme' | 'dark-theme';
+  theme: "light-theme" | "dark-theme";
   toggleTheme: () => void;
 }
 
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface ThemeProviderProps {
   children: ReactNode;
-  initialTheme?: 'light-theme' | 'dark-theme';
+  initialTheme?: "light-theme" | "dark-theme";
 }
 
-export const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+export const ThemeContext = createContext<ThemeContextProps | undefined>(
+  undefined,
+);
 
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, initialTheme }) => {
-  console.log("initialTheme", initialTheme)
-  const [theme, setTheme] = useState<'light-theme' | 'dark-theme'>(initialTheme || 'dark-theme');
-  
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  children,
+  initialTheme,
+}) => {
+  console.log("initialTheme", initialTheme);
+  const [theme, setTheme] = useState<"light-theme" | "dark-theme">(
+    initialTheme || "dark-theme",
+  );
+
   // Update theme when initialTheme prop changes, only can be changed through storybook
   useEffect(() => {
-    if (initialTheme != theme) {
-      console.log("CHANGING THEME")
+    if (initialTheme !== undefined && initialTheme != theme) {
+      console.log("CHANGING THEME");
       setTheme(initialTheme);
     }
   }, [initialTheme]);
@@ -29,12 +36,14 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, initialT
   useEffect(() => {
     const root = document.documentElement;
 
-    root.classList.remove('light-theme', 'dark-theme');
+    root.classList.remove("light-theme", "dark-theme");
     root.classList.add(`${theme}`);
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light-theme' ? 'dark-theme' : 'light-theme'));
+    setTheme((prevTheme) =>
+      prevTheme === "light-theme" ? "dark-theme" : "light-theme",
+    );
   };
 
   return (
