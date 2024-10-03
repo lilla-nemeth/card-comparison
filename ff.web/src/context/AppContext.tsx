2ccd592fdadf94709567ff71ad9c8c54cc76e485
Lilla Németh <naemly@protonmail.com>
2024-10-03 11:44:41 +0300

    fixing error of AppContextProvider in AppContext

--- ff.web/src/context/AppContext.tsx
+++ ff.web/src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 // Define the shape of your context value
 interface AppContextValue {
@@ -15,26 +15,30 @@ export const AppContext = createContext<AppContextValue | undefined>(undefined);
 export const useAppContext = () => {
   const context = React.useContext(AppContext);
   if (!context) {
-    throw new Error('useAppContext must be used within a AppContextProvider');
+    throw new Error("useAppContext must be used within a AppContextProvider");
   }
   return context;
-}
+};
 
 // Create the AppContextProvider component
-export const AppContextProvider: React.FC = ({ children } : PropTypes) => {
-  useEffect(() => { 
-    if(localStorage.getItem('profileData')) {
+export const AppContextProvider: React.FC<PropTypes> = ({
+  children,
+}: PropTypes) => {
+  useEffect(() => {
+    if (localStorage.getItem("profileData")) {
       setIsOnboardingComplete(true);
     }
-  }, [])
+  }, []);
 
   const [isOnboardingComplete, setIsOnboardingComplete] = useState(false);
-  
+
   // You can define your state and methods here
 
   // Provide the context value to the children components
   return (
-    <AppContext.Provider value={{isOnboardingComplete, setIsOnboardingComplete}}>
+    <AppContext.Provider
+      value={{ isOnboardingComplete, setIsOnboardingComplete }}
+    >
       {children}
     </AppContext.Provider>
   );
