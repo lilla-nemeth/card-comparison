5acf7d8c5165eaaa7626a4a1dca2e665154c2d7f
Lilla Németh <naemly@protonmail.com>
2024-10-03 11:52:49 +0300

    fixing missing types in AppContextValue interface

--- ff.web/src/context/AppContext.tsx
+++ ff.web/src/context/AppContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useEffect, useState } from "react";
 // Define the shape of your context value
 interface AppContextValue {
   // Add your context properties and methods here
+  isOnboardingComplete: boolean;
+  setIsOnboardingComplete: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 type PropTypes = {
