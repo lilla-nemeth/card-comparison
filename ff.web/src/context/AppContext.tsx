0b41578cc7db0220c2bda2ad615bd8ab08972870
Lilla Németh <naemly@protonmail.com>
2024-10-03 15:09:52 +0300

    Interface moved to types folder

---
+++ ff.web/src/context/AppContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useEffect, useState } from "react";
+import { AppContextValue } from "@vuo/types/contextProps";
+
+type PropTypes = {
+  children: React.ReactNode;
+};
+
+// Create the AppContext
+export const AppContext = createContext<AppContextValue | undefined>(undefined);
+
+export const useAppContext = () => {
+  const context = React.useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within a AppContextProvider");
+  }
+  return context;
+};
+
+// Create the AppContextProvider component
+export const AppContextProvider: React.FC<PropTypes> = ({
+  children,
+}: PropTypes) => {
+  useEffect(() => {
+    if (localStorage.getItem("profileData")) {
+      setIsOnboardingComplete(true);
+    }
+  }, []);
+
+  const [isOnboardingComplete, setIsOnboardingComplete] = useState(false);
+
+  // You can define your state and methods here
+
+  // Provide the context value to the children components
+  return (
+    <AppContext.Provider
+      value={{ isOnboardingComplete, setIsOnboardingComplete }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
